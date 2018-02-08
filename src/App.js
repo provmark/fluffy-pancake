8b374909d1514da7a6cb8d11b3e085138bd52e4b
Alyssa2TheRescue <alyssa2@Alyssa2TheRescue.local>
2018-02-07 18:45:49 -0800

    OMG IT RENDERS SOMETHING

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import Switch from 'react-router-dom/Switch'
+import Route from 'react-router-dom/Route'
+
 import Shell from './Shell/Shell.js'
 
 export default class App extends React.Component {
--- src/Shell/Shell.js
+++ src/Shell/Shell.js
@@ -3,13 +3,17 @@ import React from 'react'
 import { Welcome } from '../Welcome/Welcome'
 import styles from './Shell.css'
 
-export default function Shell() {
+export default class Shell extends React.Component {
+  
+  render () {
+  console.log('in shell render!');
   return (
     <div>
       <header className={styles.header}>Theme Pattern Example</header>
       <div className={styles.body}>
-      <Welcome></Welcome>
+      <h1>yayyyy!</h1>
         </div>
     </div>
   )
+}
 }
